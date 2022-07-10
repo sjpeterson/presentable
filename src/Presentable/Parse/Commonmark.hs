91d8242bdf35ae911dd4956663821b6f267a62db
Stefan Peterson <stefan.j.peterson@gmail.com>
2022-07-10 12:29:55 +0200

    add commonmark to dependencies

---
+++ src/Presentable/Parse/Commonmark.hs
@@ -0,0 +1,11 @@
+module Presentable.Parse.Commonmark where
+
+import Commonmark ( commonmark )
+
+-- commonmark :: IsBlock il bl => String -> Text -> Either ParseError bl
+--
+-- cf. parseSlideshow :: FilePath -> Text -> Either ParsingError Slideshow
+--
+-- ... so basically need to make Slideshow an instance of IsBlock and Bob's yer
+--     uncle. See (https://hackage.haskell.org/package/commonmark-0.2.2/docs/src/Commonmark.Html.html)
+--     for inspiration.
